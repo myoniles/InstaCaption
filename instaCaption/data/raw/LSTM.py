36e14a3920521a02deb5246948c40cd0d747d87b
Mike Yoniles <BLMdnst8@gmail.com>
2017-12-02 02:31:47 -0500

    cuda mem

--- instaCaption/data/raw/LSTM.py
+++ instaCaption/data/raw/LSTM.py
@@ -13,9 +13,6 @@ import torch.optim as optim
 import jsonStuff
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
-
-
-
 print("[✔️]")
 
 cudnn.benchmark = True
@@ -28,26 +25,37 @@ CNNmodel = torch.nn.DataParallel(CNNmodel).cuda()
 print("[✔️]")
 
 
-
-
-
-
-
+print(CNNmodel)
+####################################################################################################
+#   The CNN Model takes an image ( normalized below ) and returns 
+#   a 1 x 1000 float tensor
+####################################################################################################
 
 print("[ ] normalizing data...\r", end ="")
 normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 image_data = [i[0] for i in jsonStuff.Ximproved]
 Y = [i[1] for i in jsonStuff.Ximproved]
-        
+####################################################################################################
+#   first normalize the images to the CNN's specifications
+#   then makes a list of image numpy vectors.
+#   these arrays are 3 x L x W where neither L nor W are larger than 300
+####################################################################################################
 
 #plt.imshow(image_data[0])
 #plt.show()
+
+
+
 toTens = transforms.ToTensor()
 toPIL = transforms.ToPILImage()
 
-
-
-
+####################################################################################################
+#   To Tens is used to turn the Numpy arrays into pytorch tensors
+#   to PIL is used when viewing images
+#   
+#   For some reason the tensors created by ToTens are two dimensional
+#   unsqueeze is used to add an additional layer
+####################################################################################################
 image_data = [ toTens(i) for i in image_data ]
 normalize(image_data)
 image_data = [i.unsqueeze(0) for i in image_data]
@@ -59,8 +67,13 @@ image_data = [i.unsqueeze(0) for i in image_data]
 
 print("[✔️]  hihowareya?      ")
 
-
-
+####################################################################################################
+#   Custom LSTMCaption Class:
+#       takes CCN output dimensions, the output dimension, and the mapping dimension
+#       hidden returns a clear initialized hidden layer
+#       redefines foward pass
+#       moves things to GPU
+####################################################################################################
 
 
 class LSTMcaption(nn.Module):
@@ -72,28 +85,31 @@ class LSTMcaption(nn.Module):
 
         self.lstm.cuda()
         self.hidden = self.init_hidden()
-
+        #self.long_2_word = nn.Linear(hidden_dim, embedding_dim)
+        
+        
     def init_hidden(self):
-        return((Variable(torch.rand(1,1,100)), Variable(torch.randn((1,1,100)))))
+        return(Variable(torch.rand(1,1,100)).cuda(), Variable(torch.randn((1,1,100))).cuda())
     def forward(self, imageVec):
-        print(self.hidden)
-        print(imageVec)
-        outWord, self.hidden = self.lstm(imageVec, self.hidden)
-        return outWord
+        outLong, self.hidden = self.lstm(imageVec, self.hidden)
+        #Outword = self.long_2_word(outLong, -1)
+        return outLong
 
 print("[ ]  Creating LSTM...\r", end="")
 lstm = LSTMcaption(1000,100, 100)
 
 print("[✔️]")
-hidden = (Variable(torch.rand(1,1,100)), Variable(torch.randn((1,1,100))))
 
-loss_fun = nn.L1Loss()
+
+
+
+loss_fun = nn.NLLLoss()
 optimizer = optim.SGD(lstm.parameters(), lr =0.1)
 
 
 
 inputVar = Variable(image_data[0])
-print(CNNmodel(inputVar))
+#print('CNN', CNNmodel(inputVar))
 
 
 
@@ -101,30 +117,36 @@ print(CNNmodel(inputVar))
 for i in range(len(jsonStuff.Ximproved)):
     input_var = Variable(image_data[i])
     CNN_out = CNNmodel(input_var)
+    print(CNN_out)
     CNN_out = CNN_out.unsqueeze(0)
-    CNN_out.cpu()
-    print("CNN", CNN_out)
+
     lstm.zero_grad()
 
-    lstm.hidden = (Variable(torch.rand(1,1,100).cuda()), Variable(torch.randn((1,1,100)).cuda())) 
+    lstm.hidden = lstm.init_hidden() 
 
-    print(CNN_out)
     for word in Y[i]:
         temp= torch.from_numpy(word)
+        print('short temp', temp)
+        temp = temp.type(torch.LongTensor)
+        temp = temp.cuda()
+
+        print('temp', temp)
+
         target=Variable(temp, requires_grad=False)
         out = lstm(CNN_out)
+        target.type(torch.LongTensor)
+        
+        print(out[0])
+        
+        
+        
+        
 
         
-        #print(out[0])
-        out = out[0]
-        
-        
 
-        
-
-        loss = loss_fun(out, target.cpu())
-        loss.backward()
+        loss = loss_fun(out, target[0])
+        if( i == 0):
+            loss.backward(retain_graph=True)
+        else:
+            loss.backward()
         optimizer.step()
-
-
-
